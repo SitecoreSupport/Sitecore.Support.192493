70aba9cb50a60ed4f4cde6d5f0cf24d7b42f82cd
DK\dmsa <dmsa@sitecore.net>
2017-11-08 15:24:07 +0200

    The original source code

---
+++ src/Sitecore.Support.192493/Analytics/Rules/SegmentBuilder/Conditions/CountryCondition.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq.Expressions;
+using Sitecore.Analytics.Rules.SegmentBuilder;
+using Sitecore.ContentSearch.Analytics.Models;
+using Sitecore.ContentSearch.Rules.Conditions;
+using Sitecore.Diagnostics;
+
+namespace Sitecore.Support.Analytics.Rules.SegmentBuilder.Conditions
+{
+  public class CountryCondition<T> : TypedQueryableStringOperatorCondition<T, IndexedContact> where T : VisitorRuleContext<IndexedContact>
+  {
+    protected override Expression<Func<IndexedContact, bool>> GetResultPredicate(T ruleContext)
+    {
+      Assert.ArgumentNotNull(ruleContext, "ruleContext");
+
+      var contextItem = ruleContext.Item;
+      if (contextItem == null)
+      {
+        Log.Warn("Country condition ran without context item.", this);
+        return c => false;
+      }
+
+      var value = this.Value;
+      if (string.IsNullOrEmpty(value))
+      {
+        return c => false;
+      }
+
+      var item = contextItem.Database.GetItem(value);
+      if (item == null)
+      {
+        Log.Warn("Country not found in the item database: " + value, this);
+        return c => false;
+      }
+
+      value = item["Country Code"];
+
+      return this.GetCompareExpression(c => c["contact.preferredaddress.country"], value);
+    }
+  }
+}
---
+++ src/Sitecore.Support.192493/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.192493")]
+[assembly: AssemblyProduct("Sitecore.Support.192493")]
+[assembly: ComVisible(false)]
